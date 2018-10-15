2f7b08d592f547c81e933375e614354b2885fe8c
Daniel Balcells <dbalcells@gmail.com>
2018-10-15 22:31:42 +0200

    Add Django-extensions and configure shell_plus

--- quest/settings.py
+++ quest/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django-extensions settings
+SHELL_PLUS = 'ipython'
